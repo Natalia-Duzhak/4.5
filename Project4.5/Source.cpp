b360b8ca40f025fd3b8cc59c321eda70339dd652
наталя ДУЖАК <Duzhak.N@gmail.com>
2020-10-29 21:53:36 +0300

    4.5

---
+++ Project4.5/Source.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+int main()
+{
+	double x, y, R;
+	srand((unsigned)time(NULL));
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "x = "; cin >> x;
+		cout << "y = "; cin >> y;
+		cout << "R = "; cin >> R;
+		if ((y >= (sqrt((R * R) - (x + R) * (x + R)) - R) && x <= 0 && y >= 0) ||
+			(y >= -R && y <= 0 && x <= 2 * R && x >= 0))
+			cout << "yes" << endl;
+		else
+			cout << "no" << endl;
+	}
+	cout << endl << fixed;
+
+	for (int i = 0; i < 10; i++)
+	{
+		x = 6. * rand() / RAND_MAX - 3;
+		y = 6. * rand() / RAND_MAX - 3;
+			if ((y >= (sqrt((R * R) - (x + R) * (x + R)) - R) && x <= 0 && y >= 0) ||
+				(y >= -R && y <= 0 && x <= 2 * R && x >= 0))
+				cout << setw(8) << setprecision(4) << x << " "
+				<< setw(8) << setprecision(4) << y << " " << "yes" << endl
+			    << setw(8) << setprecision(4) << R << " " << "yes" << endl;
+			else
+				cout << setw(8) << setprecision(4) << x << " "
+				<< setw(8) << setprecision(4) << y << " " << "no" << endl
+			<< setw(8) << setprecision(4) << R << " " << "no" << endl;
+	}
+	return 0;
+}
